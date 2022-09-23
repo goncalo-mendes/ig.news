e94ae7c6799948b8eae11eb36193c73c68b954d9
goncalo-mendes <goncalo.mendes.dev@gmail.com>
2022-09-23 06:32:51 +0100

    initial base for the project

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import { Header } from '../components/Header'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div>macaco</div>
+    </>
+  )
+}
+
