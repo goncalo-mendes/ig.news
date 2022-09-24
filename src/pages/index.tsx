9ed4917d85c9c312d82ad08c20cbcd7aa76434d3
goncalo-mendes <goncalo.mendes.dev@gmail.com>
2022-09-24 12:21:31 +0100

    add ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { Header } from '../components/Header'
 import { SubscribeButton } from '../components/SubscribeButton'
-import { GetServerSideProps } from 'next'
+import {  GetStaticProps } from 'next'
 import styles from './home.module.scss'
 import { stripe } from '../services/stripe'
 
@@ -30,7 +30,7 @@ export default function Home({ product }: HomeProps) {
             Get acess to all the publications <br />
             <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
         </section>
         <img src='images/avatar.svg' alt='Girl coding' />
       </main>
@@ -38,7 +38,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps= async () => {
   const price = await stripe.prices.retrieve('price_1LlEH6Dlogo3V3nEwIV3HFC5', {
     expand: ['product']
   })
@@ -53,6 +53,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 , //24 hours
   }
 }
