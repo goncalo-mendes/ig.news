5b04a52f9e00512345178c9e57d2457e838f170b
goncalo-mendes <goncalo.mendes.dev@gmail.com>
2022-09-26 19:34:41 +0100

    chore: Authentication with NextAuth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import { AppProps } from 'next/app'
-import { Header } from '../components/Header'
+import { SessionProvider } from 'next-auth/react'
 
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 
 }
 
