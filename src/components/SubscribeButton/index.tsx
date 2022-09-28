7379a0ce80cc5b85e06faf4734470448f6ff0858
goncalo-mendes <goncalo.mendes.dev@gmail.com>
2022-09-28 11:07:06 +0100

    chore: Redirect to Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,15 +1,38 @@
+import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
-interface SubscribeButtonProps{
+interface SubscribeButtonProps {
   priceId: string;
 }
 
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession()
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return;
+    }
+    try {
+      const response = await api.post('/subscribe')
 
-export function SubscribeButton({priceId} : SubscribeButtonProps) {
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+
+      stripe?.redirectToCheckout({ sessionId })
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={() => handleSubscribe()}
     >
       Subscribe now
     </button >
